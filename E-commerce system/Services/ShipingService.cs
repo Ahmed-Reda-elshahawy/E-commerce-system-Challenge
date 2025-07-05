14ba2930c634dad16af9bfa9944fb043871e21f0
Ahmed-Reda-elshahawy <ahmed92.5elshahawy@gmail.com>
2025-07-05 12:35:04 +0300

    move ShipingService.cs to Services folder

--- E-commerce system/Services/ShipingService.cs
+++ E-commerce system/Services/ShipingService.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace E_commerce_system.Models
+namespace E_commerce_system.Services
 {
     public class ShipingService : IShipingService
     {
